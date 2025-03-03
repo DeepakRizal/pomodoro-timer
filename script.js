c457d33d31b74d7dec1d8de18a77102f4e78b902
Deepak Rizal <deepak2000rizal@gmail.com>
2025-03-03 16:19:04 +0530

    feat: update time when user click the start button

---
+++ script.js
@@ -0,0 +1,35 @@
+const minuteSpan = document.querySelector(".minute");
+const secondsSpan = document.querySelector(".seconds");
+const startBtn = document.querySelector(".Start");
+
+let timeLeft = 25 * 60;
+let timer;
+let isRunning = false;
+
+function updateDisplay() {
+  const minutes = Math.floor(timeLeft / 60);
+  const seconds = timeLeft % 60;
+
+  minuteSpan.textContent = String(minutes).padStart(2, "0");
+  secondsSpan.textContent = String(seconds).padStart(2, "0");
+}
+
+updateDisplay();
+
+function startTimer() {
+  if (!isRunning) {
+    isRunning = true;
+    timer = setInterval(() => {
+      if (timeLeft > 0) {
+        timeLeft--;
+        updateDisplay();
+      } else {
+        clearInterval(timer);
+        isRunning = false;
+        alert("Time's up! Take a break.");
+      }
+    }, 1000);
+  }
+}
+
+startBtn.addEventListener("click", startTimer);

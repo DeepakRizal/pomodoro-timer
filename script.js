a10e0851d25b410811d7094e600b1816da72501f
Deepak Rizal <deepak2000rizal@gmail.com>
2025-03-03 16:38:56 +0530

    feat: complete the start and reset functionality, update the UI

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const minuteSpan = document.querySelector(".minute");
 const secondsSpan = document.querySelector(".seconds");
 const startBtn = document.querySelector(".Start");
+const pauseBtn = document.querySelector(".pause");
+const resetBtn = document.querySelector(".reset");
 
 let timeLeft = 25 * 60;
 let timer;
@@ -32,4 +34,18 @@ function startTimer() {
   }
 }
 
+function pauseTimer() {
+  clearInterval(timer);
+  isRunning = false;
+}
+
+function resetTimer() {
+  clearInterval(timer);
+  isRunning = false;
+  timeLeft = 25 * 60;
+  updateDisplay();
+}
+
 startBtn.addEventListener("click", startTimer);
+pauseBtn.addEventListener("click", pauseTimer);
+resetBtn.addEventListener("click", resetTimer);
